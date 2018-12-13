d4f7f15fbd3153c31f59d73e8846bf7423f2532e
freeWillee <will.ik.lee@gmail.com>
2018-12-12 21:17:29 -0700

    Create views for projects, edit, new, index - create partials for views, moved repeat code to application view

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -13,7 +13,12 @@ class Admin::ProjectsController < ApplicationController
     redirect_to admin_projects_path
   end
 
+  def show
+    @project = Project.find(params[:id])
+  end
+
   def edit
+    @project = Project.find(params[:id])
   end
 
   def update
