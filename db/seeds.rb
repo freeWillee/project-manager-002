f3446eddaeb2141f92c2cf4382f28b3ead3e7c88
freeWillee <will.ik.lee@gmail.com>
2018-12-19 20:37:08 -0700

    clean up views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create(
+  [
+    { username: 'Mason', email: 'mason@mail.com', password: 'password'},
+    { username: 'Chloe', email: 'chloe@mail.com', password: 'password'},
+    { username: 'Seb', email: 'seb@mail.com', password: 'password'},
+    { username: 'Michael', email: 'michael@mail.com', password: 'password'},
+    { username: 'Gus', email: 'gus@mail.com', password: 'password'},
+    { username: 'Theo', email: 'theo@mail.com', password: 'password'},
+  ]
+  )
+
+projects = Project.create(
+  [
+    {name: 'Build-a-Bear', deadline: DateTime.civil(2007, 12, 4)},
+    {name: 'Treehouse', deadline: DateTime.civil(2019, 2, 4)},
+    {name: 'Fly-A-Kite', deadline: DateTime.civil(2019, 3, 4)},
+    {name: 'Theo Birthday', deadline: DateTime.civil(2019, 5, 4)}
+  ]
+)
