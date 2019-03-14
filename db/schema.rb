55948b98a6dc1eae49c8c6aee9b6078ec729ecb7
freeWillee <will.ik.lee@gmail.com>
2019-03-13 21:25:15 -0600

    Switch database to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_12_19_214854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.date "deadline"
