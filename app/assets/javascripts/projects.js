6f589ca78977cc7149c9c029bea77c33fda17524
freeWillee <will.ik.lee@gmail.com>
2019-03-16 22:28:41 -0600

    Implement show project detail JS page (admin only)

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -7,7 +7,12 @@ function listenForClick() {
     $(".js-overview").on('click', function(event){
         event.preventDefault();
         const projectId = $(this).data("id")
-        getProject(projectId)
+        // If user clicks on link content appear / disappear depending on whether or not there is content already shown.
+        if (projectContainerForId(projectId).innerText === "") {
+            getProject(projectId)
+        } else {
+            clearContainer(projectId)
+        }
     })
 }
 
@@ -19,11 +24,15 @@ function getProject(projectId) {
         method: 'GET',
         dataType: 'json'
     }).done( function(data) {
-        console.log("this is the data returned: ", data)
+        // console.logging data to test
+            console.log("this is the data returned: ", data)
         let theProject = new Project(data)
-        console.log("this is the created project JS object: ", theProject)
+            console.log("this is the created project JS object: ", theProject)
+
         let projectHTML = theProject.postHTML()
-        console.log("this is the HTML to append: ", projectHTML)
+            console.log("this is the HTML to append: ", projectHTML)
+
+        document.querySelector(`#project-overview-container-${theProject.id}`).innerHTML = projectHTML
     })
 }
 
@@ -39,10 +48,28 @@ class Project {
 
 Project.prototype.postHTML = function () {
     return (`
-        <div>
-            <p>Deadline: ${this.deadline}</p>
-            <p># of Tasks: ${this.tasks.length}</p>
+        <div style="text-align: left;">
+            <p><strong>Tasks:</strong></p>
+            ${taskTitleHTML(this.tasks)}
             <p># of people: ${this.users.length}</p>
         </div>
     `)
+}
+
+function taskTitleHTML(taskObj) {    
+    let taskHTML = "<ul>"
+    for (const task of taskObj) {
+        taskHTML += `<li><a href="/admin/projects/${task.id}">${task.title}</a></li>`
+    }
+    taskHTML +="</ul>"
+
+    return taskHTML
+}
+
+function projectContainerForId(projectId) {
+    return document.querySelector(`#project-overview-container-${projectId}`)
+}
+
+function clearContainer(projectId) {
+    projectContainerForId(projectId).innerHTML = ""
 }
