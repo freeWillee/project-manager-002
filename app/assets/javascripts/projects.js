68e62fad8027e2801687e77ad15d695f8949dffd
freeWillee <will.ik.lee@gmail.com>
2019-03-19 15:47:02 -0600

    Refactor listenforClick on projects

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -4,15 +4,19 @@ $(function() {
 });
 
 function listenForClick() {
-    $(".js-overview").on('click', function(event){
-        event.preventDefault();
-        const projectId = $(this).data("id")
-        // If user clicks on link content appear / disappear depending on whether or not there is content already shown.
-        if (projectContainerForId(projectId).innerText === "") {
-            getProject(projectId)
-        } else {
-            clearContainer(projectId)
-        }
+    $(".js-more").each(function(num, el) {
+        $(el).on('click', function(event){
+            event.preventDefault();
+            const projectId = parseInt($(el).attr("data-id"));
+            const projectDiv = $(`.project-data[data-id=${projectId}]`)
+            if (projectDiv.text() === "") {
+                getProject(projectId)
+                $(el).text("Show Less")
+            } else {
+                $(projectDiv).text("")
+                $(el).text("Show More")
+            }
+        })
     })
 }
 
@@ -32,7 +36,7 @@ function getProject(projectId) {
         let projectHTML = theProject.postHTML()
             console.log("this is the HTML to append: ", projectHTML)
 
-        document.querySelector(`#project-overview-container-${theProject.id}`).innerHTML = projectHTML
+            $(`.project-data[data-id=${projectId}]`).html(projectHTML)
     })
 }
 
@@ -69,12 +73,4 @@ function listHTML(objCollection) {
     listHTML +="</ul>"
 
     return listHTML
-}
-
-function projectContainerForId(projectId) {
-    return document.querySelector(`#project-overview-container-${projectId}`)
-}
-
-function clearContainer(projectId) {
-    projectContainerForId(projectId).innerHTML = ""
 }
