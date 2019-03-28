f95ae3bdcf02d1b83bfe09f4adb714d3c7cb1bed
freeWillee <will.ik.lee@gmail.com>
2019-03-28 09:07:42 -0600

    Modify seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,45 +9,45 @@
 users = User.create(
   [
     { username: 'admin', email: 'admin@mail.com', password: 'password', admin: 1},
-    { username: 'Mason', email: 'mason@mail.com', password: 'password'},
-    { username: 'Chloe', email: 'chloe@mail.com', password: 'password'},
-    { username: 'Seb', email: 'seb@mail.com', password: 'password'},
-    { username: 'Michael', email: 'michael@mail.com', password: 'password'},
-    { username: 'Gus', email: 'gus@mail.com', password: 'password'},
-    { username: 'Theo', email: 'theo@mail.com', password: 'password'},
+    { username: 'Melanie', email: 'mel@mail.com', password: 'password'},
+    { username: 'Jon', email: 'jon@mail.com', password: 'password'},    
+    { username: 'Crystal', email: 'crystal@mail.com', password: 'password'},    
+    { username: 'Sophia', email: 'sophia@mail.com', password: 'password'},
+    { username: 'Brad', email: 'brad@mail.com', password: 'password'},
+    { username: 'Jen', email: 'jen@mail.com', password: 'password'},
   ]
   )
 
 Project.create(
   [
-    {name: 'Build-a-Bear', deadline: Date.new(2020, 12, 4)},
-    {name: 'Treehouse', deadline: Date.new(2019, 5, 4)},
-    {name: 'Fly-A-Kite', deadline: Date.new(2019, 6, 4)},
-    {name: 'Theo Birthday', deadline: Date.new(2019, 7, 3)}
+    {name: 'Marketing Proposal', deadline: Date.new(2020, 12, 4)},
+    {name: 'Systems Conversion', deadline: Date.new(2019, 5, 4)},
+    {name: 'Budget 2020', deadline: Date.new(2019, 6, 4)},
+    {name: 'Compliance 2020', deadline: Date.new(2019, 7, 3)}
   ]
 )
 
-Task.create( 
-  [
-    { title: 'Gather Intel', content: "A horse jumped over the sheep today and a penguin said meow.", percent_complete: 30, user_id: 1, project_id: 1},
-    { title: 'Take dog for a walk', content: "Blah blah tomorrow", percent_complete: 10, user_id: 2, project_id: 2},
-    { title: 'Take pony for a walk', content: "booster seats.", percent_complete: 3, user_id: 3, project_id: 3},
-    { title: 'Brush teeth', content: "yada yada yada.", percent_complete: 90, user_id: 4, project_id: 4},
-    { title: 'Take out trash', content: "Lamps are fun to play with.", percent_complete: 40, user_id: 5, project_id: 2},
-    { title: 'Type a letter', content: "Lamps are fun to play with.", percent_complete: 30, user_id: 6, project_id: 5},
-    { title: 'Pay the bill', content: "Lamps are fun to play with.", percent_complete: 20, user_id: 7, project_id: 5},
-    { title: 'Get tires repaired', content: "Lamps are fun to play with.", percent_complete: 64, user_id: 1, project_id: 1},
-    { title: 'Paint the walls', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 4, project_id: 3},
-    { title: 'Watch the movie', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 3, project_id: 3},
-    { title: 'Sing a Song', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 6, project_id: 2},
-    { title: 'Rehearse', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 7, project_id: 1},
-    { title: 'Jumping Jacks', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 4, project_id: 4},
-    { title: 'Buy shoes', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 2, project_id: 4},
-    { title: 'Nap for 3 hrs', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 1, project_id: 5},
-    { title: 'Write a poem', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 2, project_id: 5},
-    { title: 'Advertise', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 6, project_id: 4},
-    { title: 'Email responses', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 6, project_id: 3},
-    { title: 'Tour the site', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 7, project_id: 1},
-    { title: 'Walk in the park', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 4, project_id: 3}
-  ]
-)
+# Task.create( 
+#   [
+#     { title: 'Gather Intel', content: "A horse jumped over the sheep today and a penguin said meow.", percent_complete: 30, user_id: 1, project_id: 1},
+#     { title: 'Take dog for a walk', content: "Blah blah tomorrow", percent_complete: 10, user_id: 2, project_id: 2},
+#     { title: 'Take pony for a walk', content: "booster seats.", percent_complete: 3, user_id: 3, project_id: 3},
+#     { title: 'Brush teeth', content: "yada yada yada.", percent_complete: 90, user_id: 4, project_id: 4},
+#     { title: 'Take out trash', content: "Lamps are fun to play with.", percent_complete: 40, user_id: 5, project_id: 2},
+#     { title: 'Type a letter', content: "Lamps are fun to play with.", percent_complete: 30, user_id: 6, project_id: 5},
+#     { title: 'Pay the bill', content: "Lamps are fun to play with.", percent_complete: 20, user_id: 7, project_id: 5},
+#     { title: 'Get tires repaired', content: "Lamps are fun to play with.", percent_complete: 64, user_id: 1, project_id: 1},
+#     { title: 'Paint the walls', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 4, project_id: 3},
+#     { title: 'Watch the movie', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 3, project_id: 3},
+#     { title: 'Sing a Song', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 6, project_id: 2},
+#     { title: 'Rehearse', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 7, project_id: 1},
+#     { title: 'Jumping Jacks', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 4, project_id: 4},
+#     { title: 'Buy shoes', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 2, project_id: 4},
+#     { title: 'Nap for 3 hrs', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 1, project_id: 5},
+#     { title: 'Write a poem', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 2, project_id: 5},
+#     { title: 'Advertise', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 6, project_id: 4},
+#     { title: 'Email responses', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 6, project_id: 3},
+#     { title: 'Tour the site', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 7, project_id: 1},
+#     { title: 'Walk in the park', content: "Lamps are fun to play with.", percent_complete: 10, user_id: 4, project_id: 3}
+#   ]
+# )
